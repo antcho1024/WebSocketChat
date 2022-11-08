0be106967e9a44260df93f601430f86acac146a6
hyeppy <50614113+antcho1024@users.noreply.github.com>
2022-11-08 15:59:11 +0900

    파파 2장 완료 : Stomp 로 채팅방 구현 (view 있음)
    https://daddyprogrammer.org/post/4691/spring-websocket-chatting-server-stomp-server/

--- hsChat/src/main/java/com/example/hschat/HsChatApplication.java
+++ hsChat/src/main/java/com/example/hschat/HsChatApplication.java
@@ -8,6 +8,7 @@ public class HsChatApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(HsChatApplication.class, args);
+        System.out.println("내꺼 실행완료!");
     }
 
 }
